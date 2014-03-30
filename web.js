d88e9f674998f4c3a783d0e8eb22d857901edf14
Sani005 <sanienigma@gmail.com>
2014-03-30 14:54:07 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Steve..You are the best!!!');
+  response.send('Hello World!');
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
